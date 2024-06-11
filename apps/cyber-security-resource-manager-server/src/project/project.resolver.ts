5160a502bdd54f5d0604ec5428384110f3200eeb
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2024-06-11 12:13:34 +0200

    Update project.resolver.ts

--- apps/cyber-security-resource-manager-server/src/project/project.resolver.ts
+++ apps/cyber-security-resource-manager-server/src/project/project.resolver.ts
@@ -1,4 +1,5 @@
-import { Resolver, UseGuards } from "@nestjs/common";
+import { Resolver } from "@nestjs/graphql";
+import { UseGuards } from "@nestjs/common";
 import { RolesBuilder } from "nest-access-control";
 import { ProjectService } from "./project.service";
 import { ProjectResolverBase } from "./base/project.resolver.base";
