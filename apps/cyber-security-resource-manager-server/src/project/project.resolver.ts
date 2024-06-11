d975c958a3c29d615a42d83e4a8c5ad4d3423def
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2024-06-11 12:06:12 +0200

    Update project.resolver.ts

--- apps/cyber-security-resource-manager-server/src/project/project.resolver.ts
+++ apps/cyber-security-resource-manager-server/src/project/project.resolver.ts
@@ -1,11 +1,18 @@
-import * as graphql from "@nestjs/graphql";
-import { ProjectResolverBase } from "./base/project.resolver.base";
-import { Project } from "./base/Project";
+import { Resolver, UseGuards } from "@nestjs/graphql";
+import { RolesBuilder } from "nest-access-control";
 import { ProjectService } from "./project.service";
+import { ProjectResolverBase } from "./base/project.resolver.base";
+import { GqlACGuard } from "../auth/gqlAC.guard";
+import { GqlDefaultAuthGuard } from "../auth/gqlDefaultAuth.guard";
+import { Project } from "./base/Project";
 
-@graphql.Resolver(() => Project)
+@Resolver(() => Project)
+@UseGuards(GqlDefaultAuthGuard, GqlACGuard)
 export class ProjectResolver extends ProjectResolverBase {
-  constructor(protected readonly service: ProjectService) {
-    super(service);
+  constructor(
+    protected readonly service: ProjectService,
+    protected readonly rolesBuilder: RolesBuilder
+  ) {
+    super(service, rolesBuilder);
   }
 }
