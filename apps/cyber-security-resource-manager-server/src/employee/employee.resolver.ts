0fb6b954c17a9c5540ee4c0efbebc3904c94d907
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2024-06-11 12:13:03 +0200

    Update employee.resolver.ts

--- apps/cyber-security-resource-manager-server/src/employee/employee.resolver.ts
+++ apps/cyber-security-resource-manager-server/src/employee/employee.resolver.ts
@@ -1,4 +1,5 @@
-import { Resolver, UseGuards } from "@nestjs/common";
+import { Resolver } from "@nestjs/graphql";
+import { UseGuards } from "@nestjs/common";
 import { RolesBuilder } from "nest-access-control";
 import { EmployeeService } from "./employee.service";
 import { EmployeeResolverBase } from "./base/employee.resolver.base";
